d2496f0760604ff1059df96f87f3560ec94fd877
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2023-04-11 22:34:14 -0400

    Add useLocalStorage hook

--- src/pages/portal/test.tsx
+++ src/pages/portal/test.tsx
@@ -27,25 +27,14 @@ const Test: NextPage = () => {
   const { data: session } = useSession();
 
   const [started, setStarted] = useState(false);
-  const [teamName, setTeamName] = useState(
-    localStorage.getItem("TEAM_NAME") || ""
+  const [teamName, setTeamName] = useLocalStorage("TEAM_NAME","");
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const [teamMembers, setTeamMembers] = useLocalStorage<string[]>(
+    "TEAM_MEMBER",
+    []
   );
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const savedTeamMembers = localStorage.getItem("TEAM_MEMBERS");
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const parsedTeamMembers = savedTeamMembers
-    ? JSON.parse(savedTeamMembers)
-    : [];
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const [teamMembers, setTeamMembers] = useState<string[]>(parsedTeamMembers);
   const [newMember, setNewMember] = useState("");
-
-  useEffect(() => {
-    localStorage.setItem("TEAM_MEMBERS", JSON.stringify(teamMembers));
-
-    localStorage.setItem("TEAM_NAME", teamName ? teamName : "");
-  }, [teamName, teamMembers]);
 
   const addMember = () => {
     if (newMember.trim() !== "") {
@@ -213,3 +202,43 @@ const Test: NextPage = () => {
 };
 
 export default Test;
+// Hook
+function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue] as const;
+}
