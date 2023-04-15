dee99589bdc5b95dc126f57a1ad90b8efa2e1e45
Kevin Liu <66856750+Kevin-Liu-01@users.noreply.github.com>
2023-04-15 11:18:50 -0400

    Restore getServerAuthSession

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -129,9 +129,9 @@ export const authOptions: NextAuthOptions = {
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-// export const getServerAuthSession = (ctx: {
-//   req: GetServerSidePropsContext["req"];
-//   res: GetServerSidePropsContext["res"];
-// }) => {
-//   return getServerSession(ctx.req, ctx.res, authOptions);
-// };
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
