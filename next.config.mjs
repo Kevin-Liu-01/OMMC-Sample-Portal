44f8ed0f059fee7d151f0735ab319b894272cd59
Kevin Liu <kk23907751@gmail.com>
2024-12-22 16:23:26 -0500

    [Feat] Cleanup code

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,18 @@ const config = {
       },
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
